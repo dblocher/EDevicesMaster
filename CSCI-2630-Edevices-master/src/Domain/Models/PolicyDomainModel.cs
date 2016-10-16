787ef8b00159034f6d036ee25ec411756558cf2b
mkillor <mark.killoran.jr@gmail.com>
2016-10-16 18:28:59 -0400

    "Milestone 3 last update for the Domain!"

--- CSCI-2630-Edevices-master/src/Domain/Models/PolicyDomainModel.cs
+++ CSCI-2630-Edevices-master/src/Domain/Models/PolicyDomainModel.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EDeviceClaims.Entities;
 
 namespace EDeviceClaims.Domain.Models
 {
-    public class ClaimDomainModel
+    public class PolicyDomainModel
     {
+        public PolicyDomainModel(Policy policyEntity)
+        {
+            Id = policyEntity.Id;
+            Number = policyEntity.Number;
+            SerialNumber = policyEntity.SerialNumber;
+            DeviceName = policyEntity.DeviceName;
+        }
 
+        public Guid Id { get; set; }//properties of the device(phone/tablet)
+        public string Number { get; set; }
+        public string DeviceName { get; set; }
+        public string SerialNumber { get; set; }
     }
 }
+
